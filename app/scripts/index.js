634c159f5ebc33cf6e7ea19ead4e705b94e2e52e
Dan Dietz <danddietz@gmail.com>
2017-02-09 17:51:07 -0400

    page swaping example

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,4 +1,14 @@
 var $ = require('jquery');
 var $audio = $('#audio');
+
+var splash = require('../templates/splash.hbs');
+var fight = require('../templates/fight.hbs');
+
+$('.app').html(splash());
+
+$('.js-start-game').click(function(event){
+  $('.app').html(fight());
+});
+
 $audio.attr('src', 'images/camera.mp3');
 $audio[0].play();
