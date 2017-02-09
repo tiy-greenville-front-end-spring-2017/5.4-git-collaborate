950f2789cba0c2a0df772069353276efe588fc3f
Dan Dietz <danddietz@gmail.com>
2017-02-09 18:01:04 -0400

    puppy demo

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,14 +1,15 @@
 var $ = require('jquery');
-var $audio = $('#audio');
 
-var splash = require('../templates/get_hip.hbs');
-var fight = require('../templates/puppy_list.hbs');
 
-$('.app').html(splash());
+var button = require('../templates/get_hip.hbs');
+var puppyList = require('../templates/puppy_list.hbs');
+
+$('.app').html(button());
 
 $('.js-get-puppies').click(function(event){
-  $('.app').html(fight());
+  $('.app').html(puppyList());
+  
+  var $audio = $('#audio');
+  $audio.attr('src', 'images/camera.mp3');
+  $audio[0].play();
 });
-
-$audio.attr('src', 'images/camera.mp3');
-$audio[0].play();
