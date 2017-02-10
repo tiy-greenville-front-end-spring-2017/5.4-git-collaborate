3f6fb514a3646db8d2b655a902c7041cd000ad65
Dan Dietz <danddietz@gmail.com>
2017-02-10 16:41:01 -0400

    adding radio buttons

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,15 +1,32 @@
 var $ = require('jquery');
 
-
 var button = require('../templates/get_hip.hbs');
 var puppyList = require('../templates/puppy_list.hbs');
+var $audio = $('#audio');
 
 $('.app').html(button());
 
+$(document).on('keydown', function(e){
+  if (e.keyCode == 13) {
+    showPuppies();
+  }
+});
+
 $('.js-get-puppies').click(function(event){
-  $('.app').html(puppyList());
-  
-  var $audio = $('#audio');
+  showPuppies();
+});
+
+function showPuppies(){
   $audio.attr('src', 'images/camera.mp3');
   $audio[0].play();
-});
+
+  $('.app').html(puppyList());
+
+  $('a').click(function(e){
+    e.preventDefault();
+  });
+
+  $('.puppy-selection').click(function(e){
+    console.log($('input[name="puppy"]:checked').val());
+  });
+}
